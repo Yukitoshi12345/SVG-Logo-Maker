9a856053f003384d01e267e69916ea00bbcfccf1
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-02-18 20:02:00 +1100

    triangles.js completed

--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -9,6 +9,7 @@ describe('Triangle Shape', () => {
             const shapeColour = 'yellow';
 
         const renderedSVG = triangle.render(text, textColour, shapeColour);
-        expect(renderedSVG).toBe(`<svg width='300' height='200' version='1.1' xmlns='http://www.w3.org/2000/svg'><polygon points='150,10 20,180 280,180' fill='yellow' /><text x='150' y='130' font-size='50' font-weight='bold' text-anchor='middle' fill='white'>JKL</text></svg>`);      
+        expect(renderedSVG).toBe(`<svg width='300' height='200' version='1.1' xmlns='http://www.w3.org/2000/svg'><polygon points='150,10 20,180 280,180' fill='yellow' /><text x='150' y='130' font-size='55' font-weight='bold' text-anchor='middle' fill='white'>JKL</text></svg>`);      
     });
 });
+
